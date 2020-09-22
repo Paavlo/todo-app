20e9e5340a3cb2965a900d9774072a66c19301ac
Paavlo <pavlo.com.5@gmail.com>
2020-09-22 20:08:56 +0300

    v1.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Todo App
+
+Project was created, using:
+- React.js
+- React Hooks
+- JS
+- JSX
+- CSS
+
+You can see the work of the application here - [Todo App](https://Paavlo.github.io/todo-app) 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './styles/index.css';
+import './styles/todo-list.css';
+import './styles/filters.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
